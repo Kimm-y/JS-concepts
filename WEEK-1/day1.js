82d64a7121db232751331051aa114b5b943a405d
Kimm-y <kimmiemumbi789@gmail.com>
2024-01-22 13:12:28 +0300

    learn't the day1 basics of js

---
+++ WEEK-1/day1.js
@@ -0,0 +1,90 @@
+/**
+ * @VAR
+ * [ YOU CAN DO EVERYTHING- OLDEST VERSION]
+ * you can redeclare a variable
+ * var name = " dan"
+ * var name =" James"
+ 
+ 
+ * you can reassign
+ * var name = " Dan"
+ * name = " James"
+ 
+ * @LET 
+  * You can't redeclare a variable
+  
+  * let name = " Dan"
+  * let name = " James" [ x]
+  
+  * You can reassign a variable
+  * let name = " Dan"
+  * name =" James"
+  
+  
+  * @CONST   ...IT REMAINS CONSTANT
+  
+  * You can't reassign or Redeclare  a variable
+  
+  * const name = " Dan"
+
+ */
+
+
+/** DATA TYPES - any value that can be held
+ * 
+ * Primitive data types
+ * @STRINGS = characters/numbers wrapped in quotes { "Dan" or '123'}
+ * 
+ * 
+ * @NUMBERS = decimals, whole numbers, negative numbers [ no fractions]
+ * { 1, 1.5 , -0.5}
+ * 
+ * @BOOLEANS = true or false
+ * 
+ * @UNDEFINED =  no value assigned, but expected
+ * 
+ * @NULL = no value, nothing will be added
+ * 
+ * 
+ * Non-primitive data-types
+ * 
+ * @LIST = arrays
+ * It can be a list of any of the primitive data types 
+ * [ "Dan" 1,2,3]
+ * @OBJECTS = Dictionaries [ key/value pairs]
+ */
+
+//OPERATORS
+/**
+ * @DIVISION - /
+ * @ADDITION -> +
+ * @SUBTRACTION -> -
+ * @MULTIPLICATION -> *
+ * @MODULUS -> %
+ * 
+ */
+
+
+//REVENUE
+//EXPENSES
+//COST OF GOODS SOLD
+//PROFIT
+
+const costofsock = 50
+const costofbag = 50
+const costofbatch =  (costofsock * 3) 
+const sellingprice = 1000
+const revenue = (sellingprice * 30) * 70/100
+const costofsoldsocks = costofsock * 90
+const costofsoldtBags = costofsoldsocks * 30
+
+const profit = 1000 - (500 + 200)
+
+const numberofsocks = 100
+const remainingsocks = 10
+
+
+console.log(profit)
+
+
+
